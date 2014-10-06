05b62ca8dd8bdef6998118796eef8500df26fd74
Javier Dominguez <javo20@gmail.com>
2014-10-06 19:19:54 -0400

    Add User Supplied Values to the Application

---
+++ tip-calculator2.rb
@@ -0,0 +1,18 @@
+print "Enter the cost of your meal before tax: "
+meal_cost = Float(gets)
+
+print "Enter your local tax rate: "
+tax_percent = Float(gets)
+
+print "Enter the percent tip you would like to leave: "
+tip_percent = Float(gets)
+ 
+tax_value = meal_cost * tax_percent/100
+meal_with_tax = meal_cost + tax_value
+tip_value = meal_with_tax * tip_percent/100
+total_cost = meal_with_tax + tip_value
+ 
+print "The pre-tax cost of your meal was $%.2f.\n" % meal_cost
+print "At %d%%, tax for this meal is $%.2f.\n" % [tax_percent, tax_value]
+print "For a %d%% tip, you should leave $%.2f.\n" % [tip_percent, tip_value]
+print "The grand total for this meal is then $%.2f.\n" % total_cost
